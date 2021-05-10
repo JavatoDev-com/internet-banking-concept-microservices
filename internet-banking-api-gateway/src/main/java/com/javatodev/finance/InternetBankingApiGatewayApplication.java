e55b40ca6b7b1221df0bc7a864bf25f1cfa6375d
javatodev <sendtospringboot@gmail.com>
2021-05-10 22:59:23 +0530

    api gateway

---
+++ internet-banking-api-gateway/src/main/java/com/javatodev/finance/InternetBankingApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.javatodev.finance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InternetBankingApiGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InternetBankingApiGatewayApplication.class, args);
+    }
+
+}
--- internet-banking-service-registry/src/main/java/com/javatodev/finance/InternetBankingServiceRegistryApplication.java
+++ internet-banking-service-registry/src/main/java/com/javatodev/finance/InternetBankingServiceRegistryApplication.java
@@ -2,7 +2,9 @@ package com.javatodev.finance;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+@EnableEurekaServer
 @SpringBootApplication
 public class InternetBankingServiceRegistryApplication {
 
