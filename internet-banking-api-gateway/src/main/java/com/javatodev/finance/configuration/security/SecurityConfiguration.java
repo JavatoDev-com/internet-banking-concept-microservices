9c4cb844606a6929805925e1e62c2e1cf8277038
javatodev <sendtospringboot@gmail.com>
2021-05-18 00:22:28 +0530

    api gateway security implementation done

---
+++ internet-banking-api-gateway/src/main/java/com/javatodev/finance/configuration/security/SecurityConfiguration.java
@@ -0,0 +1,28 @@
+package com.javatodev.finance.configuration.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.method.configuration.EnableReactiveMethodSecurity;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@EnableWebFluxSecurity
+@EnableReactiveMethodSecurity
+public class SecurityConfiguration {
+    @Bean
+    public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
+        http
+                .authorizeExchange()
+                //ALLOWING REGISTER API FOR DIRECT ACCESS
+                .pathMatchers("/user/api/v1/register").permitAll()
+                //ALL OTHER APIS ARE AUTHENTICATED
+                .anyExchange().authenticated()
+                .and()
+                .csrf().disable()
+                .oauth2Login()
+                .and()
+                .oauth2ResourceServer()
+                .jwt();
+        return http.build();
+    }
+}
