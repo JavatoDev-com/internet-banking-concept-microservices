346887553696cf4cd2320974ba7fc0af1d1e9a25
Chinthaka Dinadasa <sendtoclabz@gmail.com>
2021-11-19 00:14:54 +0530

    exceptions

--- internet-banking-user-service/src/main/java/com/javatodev/finance/service/KeycloakUserService.java
+++ internet-banking-user-service/src/main/java/com/javatodev/finance/service/KeycloakUserService.java
@@ -1,6 +1,7 @@
 package com.javatodev.finance.service;
 
 import com.javatodev.finance.configuration.KeycloakManager;
+import com.javatodev.finance.exception.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.keycloak.admin.client.resource.UserResource;
 import org.keycloak.representations.idm.UserRepresentation;
@@ -35,7 +36,7 @@ public class KeycloakUserService {
             UserResource userResource = keyCloakManager.getKeyCloakInstanceWithRealm().users().get(authId);
             return userResource.toRepresentation();
         } catch (Exception e) {
-            throw new RuntimeException("User not found under given ID");
+            throw new EntityNotFoundException("User not found under given ID");
         }
     }
 }
