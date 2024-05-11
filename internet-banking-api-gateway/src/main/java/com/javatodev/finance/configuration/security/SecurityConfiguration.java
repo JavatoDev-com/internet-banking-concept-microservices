c0fda3bb03ba67a358493eab440953f4f2eb3db1
Chinthaka Dinadasa <sendtoclabz@gmail.com>
2024-05-12 01:57:43 +0530

    SpringBoot 3+ Java 21 Gradle version upgrades

--- internet-banking-api-gateway/src/main/java/com/javatodev/finance/configuration/security/SecurityConfiguration.java
+++ internet-banking-api-gateway/src/main/java/com/javatodev/finance/configuration/security/SecurityConfiguration.java
@@ -1,6 +1,8 @@
 package com.javatodev.finance.configuration.security;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.method.configuration.EnableReactiveMethodSecurity;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
@@ -9,20 +11,26 @@ import org.springframework.security.web.server.SecurityWebFilterChain;
 @EnableWebFluxSecurity
 @EnableReactiveMethodSecurity
 public class SecurityConfiguration {
+
+    @Value("${spring.security.oauth2.resourceserver.jwt.jwk-set-uri}")
+    private String jwkUri;
+
     @Bean
     public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
-        http
-                .authorizeExchange()
-                //ALLOWING REGISTER API FOR DIRECT ACCESS
-                .pathMatchers("/user/api/v1/register").permitAll()
-                //ALL OTHER APIS ARE AUTHENTICATED
-                .anyExchange().authenticated()
-                .and()
-                .csrf().disable()
-                .oauth2Login()
-                .and()
-                .oauth2ResourceServer()
-                .jwt();
-        return http.build();
+
+        ServerHttpSecurity httpSecurity = http
+            .authorizeExchange(exchanges ->
+                //ALLOW OPTIONS REQUEST FOR CORS CONFIG ANGULAR APPLICATION
+                exchanges.pathMatchers("/actuator/**").permitAll()
+                    .pathMatchers("/user/api/v1/register").permitAll()
+                    .anyExchange().authenticated()
+            );
+
+        httpSecurity.csrf(ServerHttpSecurity.CsrfSpec::disable);
+
+        httpSecurity.oauth2ResourceServer(oAuth2ResourceServer -> oAuth2ResourceServer.jwt(jwt -> jwt.jwkSetUri(jwkUri)));
+
+        return httpSecurity.build();
+
     }
 }
