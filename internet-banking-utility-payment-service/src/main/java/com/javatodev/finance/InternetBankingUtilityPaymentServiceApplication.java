d5565b1519a64e67adf69da53e5efc992a09af82
Chinthaka Dinadasa <sendtoclabz@gmail.com>
2024-05-13 16:38:28 +0530

    utility service version upgraded, docker-compose init

--- internet-banking-utility-payment-service/src/main/java/com/javatodev/finance/InternetBankingUtilityPaymentServiceApplication.java
+++ internet-banking-utility-payment-service/src/main/java/com/javatodev/finance/InternetBankingUtilityPaymentServiceApplication.java
@@ -2,16 +2,14 @@ package com.javatodev.finance;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@EnableEurekaClient
 @EnableFeignClients
 @SpringBootApplication
 public class InternetBankingUtilityPaymentServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(InternetBankingUtilityPaymentServiceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(InternetBankingUtilityPaymentServiceApplication.class, args);
+    }
 
 }
