cfacda56e3a0b81890aa1efcc9ede13af4812b5d
teja2 <mrteja24@gmail.com>
2025-11-03 14:20:15 -0500

    swagger setup for payment controller

--- internet-banking-utility-payment-service/src/main/java/com/javatodev/finance/controller/UtilityPaymentController.java
+++ internet-banking-utility-payment-service/src/main/java/com/javatodev/finance/controller/UtilityPaymentController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import lombok.RequiredArgsConstructor;
 
+@Tag(name = "Utility Payment API", description = "API for processing utility payments")
 @RestController
 @RequiredArgsConstructor
 @RequestMapping(value = "/api/v1/utility-payment")
@@ -20,11 +21,13 @@ public class UtilityPaymentController {
 
     private final UtilityPaymentService utilityPaymentService;
 
+    @Operation(summary = "Read Utility Payments", description = "Retrieve a paginated list of utility payments")
     @GetMapping
     public ResponseEntity readPayments(Pageable pageable) {
         return ResponseEntity.ok(utilityPaymentService.readPayments(pageable));
     }
 
+    @Operation(summary = "Process Utility Payment", description = "Process a utility payment request")
     @PostMapping
     public ResponseEntity processPayment(@RequestBody UtilityPaymentRequest paymentRequest) {
         return ResponseEntity.ok(utilityPaymentService.utilPayment(paymentRequest));
